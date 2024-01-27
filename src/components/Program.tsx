de668c33bb88f38634c5de8dcb3e261213b28f5b
farozy <farozy12@gmail.com>
2024-01-28 00:40:24 +0700

    solve the page program

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -26,7 +26,7 @@ export default function Program() {
     ];
 
     return (
-        <section className="bg-gray-400 antialiased text-gray-900 px-5 pt-5 pb-14" id="program">
+        <section className="bg-gray-400 antialiased text-gray-900 px-5 pt-5 pb-16" id="program">
             <div className="title text-center text-4xl pb-4 relative font-semibold">
                 Program
             </div>
